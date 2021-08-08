fafb703d08327ea76ac0bf5e5337f1e8cdabd135
Luke Jacobsen <lukejacobsen@gmail.com>
2021-08-08 12:33:38 +0100

    added routing and home & about pages

---
+++ src/components/about/about.component.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./about.component.css";
+
+export class AboutPage extends Component {
+  render() {
+    return <div>About Time</div>;
+  }
+}
--- src/components/nav-bar/nav-bar.component.tsx
+++ src/components/nav-bar/nav-bar.component.tsx
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import "./nav-bar.component.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export class NavBar extends Component {
   render() {
     return (
       <nav className="nav-bar-container">
-        <a href="/home" className="nav-bar-item">
+        <Link to="/" className="nav-bar-item">
           Home
-        </a>
-        <a href="/about" className="nav-bar-item">
+        </Link>
+        <Link to="/about" className="nav-bar-item">
           About
-        </a>
+        </Link>
       </nav>
     );
   }
