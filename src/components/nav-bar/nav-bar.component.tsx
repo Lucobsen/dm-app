a8bd2fbb96843d77db2e2b5d97261b41499e662f
Luke Jacobsen <lukejacobsen@gmail.com>
2021-08-08 11:51:04 +0100

    added prettier

--- src/components/nav-bar/nav-bar.component.tsx
+++ src/components/nav-bar/nav-bar.component.tsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
-import './nav-bar.component.css';
+import "./nav-bar.component.css";
 
 export class NavBar extends Component {
   render() {
     return (
       <nav className="nav-bar-container">
-        <a href="/home" className="nav-bar-item">Home</a>
-        <a href="/about" className="nav-bar-item">About</a>
+        <a href="/home" className="nav-bar-item">
+          Home
+        </a>
+        <a href="/about" className="nav-bar-item">
+          About
+        </a>
       </nav>
-    )
+    );
   }
 }
